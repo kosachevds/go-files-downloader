bfbaae07c792b360941dcf52b8f31d473d3d1153
Kosachev Daniil <kos-dan@mail.ru>
2019-09-22 12:41:21 +0300

    New module for downloading

---
+++ internal/downloader/gownloader.go
@@ -0,0 +1,25 @@
+package downloader
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
+)
+
+func DownloadFile(url string) ([]byte, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.New(response.Status)
+	}
+	var data bytes.Buffer
+	_, err = io.Copy(&data, response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return data.Bytes(), nil
+}
